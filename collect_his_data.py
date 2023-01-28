406d6e29d8b130ebaf87031076454e38447c6700
Mathew Miller <mathewpqmiller@gmail.com>
2023-01-28 11:14:35 -0800

    #32 - Collecting more data - collection scrip 2

--- collect_his_data.py
+++ collect_his_data.py
@@ -11,11 +11,31 @@ INCREMENTS = {
     'H4' : 240
 }
 
+def create_file(pair, granularity, api):
+    candle_count = 2000
+    time_step = INCREMENTS[granularity] * candle_count
+    
+    end_date = utils.get_utc_dt_from_string("2022-12-31 23:59:59")
+    date_from = utils.get_utc_dt_from_string("2020-01-01 00:00:00")
+    
+    candle_dfs = []
+    
+    date_to = date_from
+    while date_to < end_date:
+        date_to = date_from + dt.timedelta(minutes=time_step)
+        if date_to > end_date:
+            date_to = end_date
+        print(date_from, date_to)
+        # collect candles
+        date_from = date_to
+        
 def run_collection():
     pair_list = "GBP,EUR,USD,CAD,JPY,NZD,CHF"
+    api = OandaAPI()
     for g in INCREMENTS.keys():
         for i in Instrument.get_pairs_from_string(pair_list):
             print(g, i)
+            create_file(i, g, api)
             
 if __name__ == "__main__":
     run_collection()
