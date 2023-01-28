0a25590ae5a7a5f20386a607628a1d44c0fa1693
Mathew Miller <mathewpqmiller@gmail.com>
2023-01-28 07:19:57 -0800

    #30 - Collecting more data - date utilities in Python

--- oanda_api.py
+++ oanda_api.py
@@ -27,19 +27,30 @@ class OandaAPI():
         if df is not None:
             df.to_pickle(utils.get_instruments_data_filename())
     
-    def fetch_candles(self, pair_name, count, granularity):
+    def fetch_candles(self, pair_name, count=None, granularity="H1", date_from=None, date_to=None):
         url = f"{defs.OANDA_URL}/instruments/{pair_name}/candles"
         
         params = dict(
-            count = count,
             granularity = granularity,
             price = "MBA"
         )
         
+        if date_from is not None and date_to is not None:
+            params['to'] = int(date_to.timestamp())
+            params['from'] = int(date_from.timestamp())
+        elif count is not None:
+            params['count'] = count
+        else:
+            params['count'] = 300
+            
         response = self.session.get(url, params=params, headers=defs.SECURE_HEADER) 
         
         return response.status_code, response.json() 
         
+
 if __name__ == "__main__":
     api = OandaAPI()
-    api.save_instruments()
+    # api.save_instruments()
+    date_from = utils.get_utc_dt_from_string("2019-05-05 18:00:00")
+    date_to = utils.get_utc_dt_from_string("2019-05-10 18:00:00")
+    print(api.fetch_candles("EUR_USD", date_from=date_from, date_to=date_to))
