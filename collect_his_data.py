43c2aac655fae5e7be7503391d7dd1bbb1f045f5
Mathew Miller <mathewpqmiller@gmail.com>
2023-01-30 05:22:39 -0800

    #45 - Updating candle data collection

--- collect_his_data.py
+++ collect_his_data.py
@@ -11,24 +11,6 @@ INCREMENTS = {
     'H4' : 240
 }
 
-def get_candles_df(json_response):
-    
-    prices = ['mid', 'bid', 'ask']
-    ohlc = ['o', 'h', 'l', 'c']
-    
-    our_data = []
-    for candle in json_response['candles']:
-        if candle['complete'] == False:
-            continue
-        new_dict = {}
-        new_dict['time'] = candle['time']
-        new_dict['volume'] = candle['volume']
-        for price in prices:
-            for oh in ohlc:
-                new_dict[f"{price}_{oh}"] = candle[price][oh]
-        our_data.append(new_dict)
-    return pd.DataFrame.from_dict(our_data)
-
 def create_file(pair, granularity, api):
     candle_count = 2000
     time_step = INCREMENTS[granularity] * candle_count
@@ -44,12 +26,13 @@ def create_file(pair, granularity, api):
         if date_to > end_date:
             date_to = end_date
         
-        code, json_data = api.fetch_candles(pair,
+        code, df = api.fetch_candles(pair,
                 granularity=granularity,
                 date_from=date_from,
-                date_to=date_to)
-        if code == 200 and len(json_data['candles']) > 0:
-            candle_dfs.append(get_candles_df(json_data))
+                date_to=date_to,
+                as_df=True)
+        id df is not None and df.empty == False:
+            candle_dfs.append(df)    
         elif code != 200:
             print("ERROR", pair, granularity, date_from, date_to)
             break
--- oanda_api.py
+++ oanda_api.py
@@ -1,8 +1,10 @@
 
 import requests
 import pandas as pd
+from dateutil.parser import * 
 import defs
 import utils
+import sys
 
 class OandaAPI():
     
@@ -27,7 +29,7 @@ class OandaAPI():
         if df is not None:
             df.to_pickle(utils.get_instruments_data_filename())
     
-    def fetch_candles(self, pair_name, count=None, granularity="H1", date_from=None, date_to=None):
+    def fetch_candles(self, pair_name, count=None, granularity="H1", date_from=None, date_to=None, as_df=False):
         url = f"{defs.OANDA_URL}/instruments/{pair_name}/candles"
         
         params = dict(
@@ -48,12 +50,36 @@ class OandaAPI():
         if response.status_code != 200:
             return response.status_code, None
         
-        return response.status_code, response.json() 
+        if as_df == True:
+            json_data = response.json()['candles']
+            return response.status_code, OandaAPI.candles_to_df(json_data)
+        else:
+            return response.status_code, response.json() 
+
+    @classmethod
+    def candles_to_df(cls, json_data):
+        prices = ['mid', 'bid', 'ask']
+        ohlc = ['o', 'h', 'l', 'c']
         
+        our_data = []
+        for candle in json_data:
+            if candle['complete'] == False:
+                continue
+            new_dict = {}
+            new_dict['time'] = candle['time']
+            new_dict['volume'] = candle['volume']
+            for price in prices:
+                for oh in ohlc:
+                    new_dict[f"{price}_{oh}"] = float(candle[price][oh])
+            our_data.append(new_dict)
+        df = pd.DataFrame.from_dict(our_data)
+        df["time"] = [parse(x) for x in df.time]
+        return df
 
 if __name__ == "__main__":
     api = OandaAPI()
     # api.save_instruments()
     date_from = utils.get_utc_dt_from_string("2019-05-05 18:00:00")
     date_to = utils.get_utc_dt_from_string("2019-05-10 18:00:00")
-    print(api.fetch_candles("EUR_USD", date_from=date_from, date_to=date_to))
+    res, df = api.fetch_candles("EUR_USD", date_from=date_from, date_to=date_to, as_df=True)
+    print(df.info())
