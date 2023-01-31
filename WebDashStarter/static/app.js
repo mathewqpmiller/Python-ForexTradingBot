712c8100df9cc3d5ec94b22580c22fb1ec5ae91a
Mathew Miller <mathewpqmiller@gmail.com>
2023-01-31 05:06:23 -0800

    #58 - Indicator Web Dash #7 - Styling the KPI table

--- WebDashStarter/static/app.js
+++ WebDashStarter/static/app.js
@@ -10,6 +10,22 @@ var app = new Vue({
             const response = await fetch('data.json');
             const data = await response.json();
             this.kpiData = data;
+        },
+        applyOnOffClass: function(val) {
+            if(val === 0 || val === false) {
+                return ""
+            } else {
+                return 'alert_green';
+            }
+        },
+        applyDirectionClass: function(val) {
+            if( val === 0) {
+                return"";
+            } else if (val === 1) {
+                return 'alert_green';
+            } else if (val === -1) {
+                return 'alert_red';
+            }
         }
     }
   })
