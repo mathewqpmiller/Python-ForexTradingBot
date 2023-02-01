73d33d890804acf90e7769159606ef1059a7db50
Mathew Miller <mathewpqmiller@gmail.com>
2023-02-01 06:21:45 -0800

    #64 - Live bot - Making a trade

---
+++ TradingBotStarter/defs.py
@@ -0,0 +1,8 @@
+API_KEY = "475688acdafd540bb5cefbc4598320ce-9d23b71c959601a2b046590bc09df83d"
+ACCOUNT_ID = "101-001-13800215-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}',
+    'Content-Type': 'application/json'
+}
